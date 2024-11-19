a5e7c1191b9b8a70f56d8f1160aeb592a76fd069
Mai Rodrigo <maialrodrigo@gmail.com>
2024-11-18 21:41:25 -0300

    Reestructuracion del proyecto: muevo app.js a src

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+// Importar enrutadores
+import productsRouter from "./routes/productsRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+
+// Se crea una instancia de la aplicación Express
+const app = express();
+
+// Se define el puerto en el que el servidor escuchará las solicitudes
+const PORT = 8080;
+
+// Middleware para acceder al contenido JSON de las solicitudes
+app.use(express.json());
+
+// Declaración de rutas para los productos y carritos
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartRouter);
+
+// Ruta raíz con mensaje de bienvenida
+app.get("/", (req, res) => {
+	res.send(
+		"Bienvenido a la API de Pazencia. Utilice /api/products o /api/carts para acceder a los datos."
+	);
+});
+
+// Se levanta el servidor oyendo en el puerto solicitado
+app.listen(PORT, () => {
+	console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
+});
